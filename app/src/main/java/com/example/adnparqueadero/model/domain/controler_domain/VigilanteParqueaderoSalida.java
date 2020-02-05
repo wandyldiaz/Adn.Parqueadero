d101cfc0acda71df19d0f21364a3eda894e5d46a
wandyldiaz <60435233+wandyldiaz@users.noreply.github.com>
2020-02-05 16:52:22 -0500

    Update VigilanteParqueaderoSalida.java

--- app/src/main/java/com/example/adnparqueadero/model/domain/controler_domain/VigilanteParqueaderoSalida.java
+++ app/src/main/java/com/example/adnparqueadero/model/domain/controler_domain/VigilanteParqueaderoSalida.java
@@ -7,14 +7,10 @@ public class VigilanteParqueaderoSalida extends VigilanteParqueadero {
 
 
     public static long getDifferenceBetwenDates(Date dateInicio, Date dateFinal) {
-
-
-
         long milliseconds = dateFinal.getTime() - dateInicio.getTime();
         int seconds = (int) (milliseconds / 1000) % 60;
         int minutes = (int) ((milliseconds / (1000 * 60)) % 60);
         int hours =(int) milliseconds / (1000 * 60 * 60);
-
         return (minutes+(hours*60));
     }
 }
